971c7838d4bec026e938a40b3505820bd3190216
Sergey Manzhak <manzhak.ru@gmail.com>
2019-07-30 18:43:41 +0300

    Clean folders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,12 @@ window.App = {};
 
 window.App.popup = Popup();
 window.App.select = Select();
-window.App.stations = Stations();
-
 
 window.App.popup.init({
     button: 'modal-button',
     content: 'modal-content',
 });
 
-/*
-window.App.ajax.init({
-    url: 'php/getData.php',
-    table: 'kp',
-    param: '',
-});
-
- */
 
 
 
