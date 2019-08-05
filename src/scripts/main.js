d505b7460961059c7681a5e3af4fd34686b41d5b
Sergey Manzhak <manzhak.com@gmail.com>
2019-08-05 14:58:56 +0300

    Fix some issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,17 @@
 'use strict';
-(function() {
 
-    window.App = {};
+window.App = {};
 
-    window.App.popup = Popup();
-    window.App.select = Select();
-    window.App.search = Search();
+window.App.popup = Popup();
+window.App.select = Select();
+window.App.search = Search();
 
-    window.App.popup.init({
-        button: '#modal-button',
-        title: 'Импульсы ТС',
-        content: 'Импульс 0-есть сигнализация, 1-нет сигнализации.'
-    });
+window.App.popup.init({
+    button: '#modal-button',
+    title: 'Импульсы ТС',
+    content: 'Импульс 0-есть сигнализация, 1-нет сигнализации.'
+});
 
-})();
 
 
 
