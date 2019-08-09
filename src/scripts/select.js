2c6905512e8fc79899e40cf8dfacc5f8ff56625e
Sergey Manzhak <manzhak.ru@gmail.com>
2019-08-09 19:33:07 +0300

    Add mnemo id

--- src/scripts/select.js
+++ src/scripts/select.js
@@ -76,7 +76,7 @@ const Select = function() {
                         data.forEach(function (item) {
                             let div = document.createElement('div');
                             div.innerHTML = item.sign + ' - ' + item.dev_desc + ' ' + item.place;
-                            div.innerHTML += `<span class="info">&angrt; id: ${item.val_id}, ip: ${item.interface}, id_shem:${item.id_shem}</span>`;
+                            div.innerHTML += `<span class="info">&angrt; id: ${item.val_id}, ip: ${item.interface}, id_shem: ${item.id_shem}, id_mnemo: ${item.mnemo_id}</span>`;
                             if(item.dev_desc == '0') {
                                 div.className ='alarm';
                             }
